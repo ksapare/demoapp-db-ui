1c29274757cb6bf9c053dcad8a8449942a5f36e4
Krishna Sapare <johndoe@example.com>
2023-02-09 16:45:42 +0530

    modified the address

--- src/pages/testcloud/testcloud.component.jsx
+++ src/pages/testcloud/testcloud.component.jsx
@@ -7,7 +7,9 @@ export default function TestCloud(){
     const [response, setResponse] = useState([])
 
     function handleClick(){
-        axios.get("http://localhost:8080/v1/items/")
+        //axios.get("http://localhost:8080/v1/items/")
+        axios.get("http://ldemoapp-db-dev.up.railway.app/v1/items/")
+        
         .then((resp) =>setResponse(resp.data))
     }
 
