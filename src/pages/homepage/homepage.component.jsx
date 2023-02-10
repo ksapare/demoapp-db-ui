95e363a26f6c9082db4f15df9c4109c6d759c7e7
Krishna Sapare <johndoe@example.com>
2023-02-10 11:11:11 +0530

    corrected the API end point

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { CardList } from '../../components/card-list/card-list.component';
 import { SearchBox } from '../../components/search-box/search-box.component';
-
-
 
 export default function HomePage() {
     
